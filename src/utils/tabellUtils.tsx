ad1f88fba31a49c56cad427b034543c5bfe02e20
sarahjelle <sarahj.98@hotmail.com>
2022-09-19 12:20:58 +0200

    Fjern kommentar

--- src/utils/tabellUtils.tsx
+++ src/utils/tabellUtils.tsx
@@ -1,7 +1,6 @@
-import React from "react";
-import { renderToString } from "react-dom/server";
+import React from 'react';
+import { renderToString } from 'react-dom/server';
 
-//Kommentar legges til for testing
 export const listeTilHtmlTabell = (tabell: string[][]) => {
 	return renderToString(
 		<table>
