c3e61d56915ea96d4fea38de8cbcfe511fbf5f4b
sarahjelle <sarahj.98@hotmail.com>
2022-10-06 12:51:29 +0200

    Mellomlagring

--- src/komponenter/delseksjon.tsx
+++ src/komponenter/delseksjon.tsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { Checkbox } from '@navikt/ds-react';
+import { Button, Checkbox } from '@navikt/ds-react';
 import { SkjemaContext } from '../context/context';
 import { SanityDelseksjon, SanityDropdown, SanityTekstObjekt } from '../typer/sanity';
 import { Fritekst } from './fritekst';
@@ -112,6 +112,7 @@ export function Delseksjon({
     };
 
     const håndterEndringIDropdown = (nyTekstOgIndeksStreng: string, innholdIndeks: number) => {
+        console.log(nyTekstOgIndeksStreng);
         const nyTekstOgIndeks = nyTekstOgIndeksStreng.split('@&#');
         const valgIndeks = Number(nyTekstOgIndeks[1]);
         const nyFritekstTabell = oppdaterFritekstTabellMedDropdown(
@@ -164,35 +165,56 @@ export function Delseksjon({
         console.log('nullstill fritekstfelt'); // Må håndtere flettefelt, disse skal vell ikke slettes?
         // Finn initiell tekst til fritekst-tabellen
         // settFritekst('');
-        const nyFritekstTabell = innholdTilFritekstTabell(delseksjon.innhold);
+        const nyFritekstTabell = innholdTilFritekstTabell(delseksjon.innhold); //Kun for å få struktur
+
         // Tilbakestill tekst knyttet til dropdowns
         delseksjonerState[delseksjonIndeks].innhold.forEach(
             (innhold: string[] | dropdown, indeks: number) => {
                 if ((innhold as dropdown).valgVerdi != undefined) {
-                    nyFritekstTabell[indeks] = [
-                        (innhold as dropdown).valgVerdi?.split('@&#')[0] || '',
-                    ];
+                    nyFritekstTabell[indeks] = (innhold as dropdown).valgVerdi
+                        ?.split('@&#')[0]
+                        .split('|') || [''];
+                    if ((innhold as dropdown).flettefelt.length > 0) {
+                        let flettefeltIndeks = 0;
+                        for (let i = 1; i < nyFritekstTabell[indeks].length; i += 2) {
+                            nyFritekstTabell[indeks][i] = (innhold as dropdown).flettefelt[
+                                flettefeltIndeks
+                            ];
+                            console.log(i);
+                        }
+                    }
+                } else if (innhold as string[]) {
                 }
             }
         );
 
-        // settFritekstTabell(nyFritekstTabell);
-        // const nyDelseksjonState = oppdaterFritekstTabellIMellomlagring(
-        //     { ...mellomlagringDelseksjonerState[delseksjonIndeks] },
-        //     nyFritekstTabell
+        console.log('ny fri: ', nyFritekstTabell);
+        //for (let i = 1; i + 2; i < nyFritekstTabell.length) {}
+
+        // Fyll inn flettefeltverdier
+        // const nyFlettefeltTabell = innholdTilFlettefeltTabell(
+        //     delseksjon.innhold,
+        //     delseksjonerState[delseksjonIndeks].innhold
         // );
-        // settOppdaterFritekst(true);
-        // const nyFritekst = dobbelTabellTilStreng(nyFritekstTabell);
-        // console.log(dobbelTabellTilStreng(nyFritekstTabell));
-        // // settFritekst(dobbelTabellTilStreng(nyFritekstTabell));
-        // // oppdaterAvsnitt(nyFritekst);
-        // setMellomlagringDelseksjonState(nyMellomlagringDelseksjon);
-        // //setMellomlagringDelseksjonState(nyMellomlagringDelseksjon);
-        // // mellomlagret[delseksjonsindeks].innhold = [[], {flettefelt: [], valgverdi: ""}]
-        // // [] om det er en tekst
-        // // {flettefelt, valgverdi} om det er tekst fra dropdown.
-        // console.log('Mellomlagret: ', nyMellomlagringDelseksjon);
-        // console.log('nullstill fritekst');
+
+        //console.log(nyFritekstTabell);
+        //settFlettefelt(nyFlettefeltTabell);
+
+        settFritekstTabell(nyFritekstTabell);
+        const nyDelseksjonState = oppdaterFritekstTabellIDelseksjonState(
+            { ...delseksjonerState[delseksjonIndeks] },
+            nyFritekstTabell
+        );
+        settOppdaterFritekst(true);
+        // settFritekst(dobbelTabellTilStreng(nyFritekstTabell));
+        // oppdaterAvsnitt(nyFritekst);
+        settDelseksjonerState(nyDelseksjonState);
+        //setMellomlagringDelseksjonState(nyMellomlagringDelseksjon);
+        // mellomlagret[delseksjonsindeks].innhold = [[], {flettefelt: [], valgverdi: ""}]
+        // [] om det er en tekst
+        // {flettefelt, valgverdi} om det er tekst fra dropdown.
+        console.log('Mellomlagret: ', nyDelseksjonState);
+        console.log('nullstill fritekst');
     };
 
     return (
@@ -275,6 +297,9 @@ export function Delseksjon({
                         oppdaterFritekst={oppdaterFritekst}
                         settOppdaterFritekst={settOppdaterFritekst}
                     />
+                    <div className="nullstill-fritekst-kontainer">
+                        <Button onClick={nullstillFritekst}>Tilbakestill fritekstfelt</Button>
+                    </div>
                 </div>
             )}
         </div>
