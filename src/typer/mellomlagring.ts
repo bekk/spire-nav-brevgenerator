07b01986ce21bd2b065bbcd7bd57255b981c1a73
sarahjelle <sarahj.98@hotmail.com>
2022-10-07 09:53:23 +0200

    Fjernet avsnitt fra mellomlagring

--- src/typer/mellomlagring.ts
+++ src/typer/mellomlagring.ts
@@ -3,6 +3,5 @@ import { StateDelseksjon } from './typer';
 export interface mellomlagringState {
     brevmalId: string;
     inkluderingsbrytere: boolean[];
-    avsnitt: string[];
     delseksjoner: StateDelseksjon[];
 }
